37defa5dcd67c93f0315a25908d750ce0f0fd512
YoungMercy808 <100361086+flaviopfister@users.noreply.github.com>
2024-04-09 11:57:20 +0200

    initial push

---
+++ guardian_manager.py
@@ -0,0 +1,50 @@
+from idun_guardian_sdk import GuardianBLE, GuardianAPI
+from datetime import datetime, timezone, timedelta
+import time
+import datetime
+
+class GuardianManager:
+    def __init__(self, device_id, api_key):
+        self.ble = GuardianBLE()
+        self.api = GuardianAPI(device_id, api_key)
+        self.files = []
+
+    def check_battery(self):
+        print(f"Battery: {self.ble.get_battery()}%")
+
+    def check_impedance(self):
+        self.ble.start_impedance(lambda data: print(f"{data}\thOhm"))
+        time.sleep(10)
+        self.ble.stop_impedance()
+
+    def stream_data(self, duration, callback=lambda data: data):
+        start_time = datetime.datetime.now()
+        recording_id = self.api.start_recording(callback, filtered_stream=True, raw_stream=False)
+        self.ble.start_recording(self.api.callback)
+        while datetime.datetime.now() - start_time < datetime.timedelta(seconds=duration):
+            pass
+
+    def stream_data_jaw(self, callback=lambda data: data):
+        data = self.api.get_recordings()    
+        ongoing_recording_id = None
+        # Überprüfen, ob 'status' == 'ONGOING' für jedes Element in den Daten
+        for entry in data:
+            if entry.get('status', {}).get('status') == 'ONGOING':
+                ongoing_recording_id = entry['recordingID']
+                self.api.stop_recording(ongoing_recording_id)
+        #self.ble.stop_recording()
+        recording_id = self.api.start_recording(None, filtered_stream=True, raw_stream=False)
+        self.ble.start_recording(self.api.callback)
+        self.api.subscribe_rt(['JAW_CLENCH'], lambda data: print(data))
+        while True:
+            pass
+
+    def disconnect_session(self):
+        self.api.stop_recording()
+        self.ble.stop_recording()
+        self.ble._disconnect()
+        print("Disconnected")
+
+    def recoridngs(self):
+        recorings = self.api.get_recordings()
+        return print(recorings)
---
+++ jaw_clenches.py
@@ -0,0 +1,16 @@
+from guardian_manager import GuardianManager
+
+def callback(data):
+    print(data)
+    #print(data['ch1'])
+    pass
+
+gm = GuardianManager(
+    device_id="D6-96-7C-C5-2E-14",
+    api_key="idun_2zZuE2ImqssU8T0HnVOlkJNutPXc6q_9nAt0DpTNtrAYgcocAHhEbxoI"
+)
+
+#gm.recoridngs()
+#gm.stream_data(5, callback)
+#gm.disconnect_session()
+gm.stream_data_jaw(callback)
